64285277bfbfd00326c505dbc875b8569413b9da
bokkunbokkun <sugan100@hanmail.net>
2016-06-27 23:24:30 +0900

    practice206.java
    chapter 2 question number 6

---
+++ practice206.java
@@ -0,0 +1,13 @@
+public class practice206{
+	
+	public static void main(String[] args){
+		int age;
+		float f = 0.25f;
+		double d = age + (double) f;
+		char c = 'a';
+		String name = "eunjin";
+		
+		
+	}
+
+}
